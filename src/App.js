9714a94df6131a411875b69f4ad8c8589b78afcc
Fernando Lima <fernandojvlima@gmail.com>
2019-10-08 21:02:53 -0300

    proxy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import Home from './Home';
+import axios from 'axios';
 import Genero from './Genero';
 import Series from './Series';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios
+      .get('/api')
+      .then(res => {
+        setData(res.data)
+      })
+  }, [])
   return (
     <Router>
       <div>
         <Header />
         <Switch>
+
           <Route path='/' exact component={Home} />
           <Route path='/generos' exact component={Genero} />
           <Route path="/series" exact component={Series} />
         </Switch>
+        <pre>{JSON.stringify(data)}</pre>
       </div>
     </Router>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+
+
 
 const Home = () => {
+
+
   return (
     <div className="container">
       <h1>Home</h1>
+      <pre></pre>
     </div>
   )
 }
